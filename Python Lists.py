dd789b8f0defcfcb605dde6030cc51d18decdf0a
Nick B <bunsni@gmail.com>
2020-12-07 11:44:22 +0000

    Medical insurance project update

--- Python Lists.py
+++ Python Lists.py
@@ -129,3 +129,26 @@ def middle_element(lst):
         return lst[int(x/2)]
 
 print(middle_element([5, 2, -10, -4, 4, 5]))
+
+
+#Medical Insurance Project
+names = ["Mohamed", "Sara", "Xia", "Paul", "Valentina", "Jide", "Aaron", "Emily", "Nikita", "Paul"]
+insurance_costs = [13262.0, 4816.0, 6839.0, 5054.0, 14724.0, 5360.0, 7640.0, 6072.0, 2750.0, 12064.0]
+
+names.append('Priscilla')
+insurance_costs.append(8320.0)
+
+medical_records = list(zip(insurance_costs, names))
+print(medical_records)
+num_medical_records = len(medical_records)
+print('There are ', num_medical_records, ' medical records')
+first_medical_record = medical_records[0]
+print('Here is the first medical record: ', first_medical_record)
+medical_records.sort()
+print('Here are the medical records sorted by insurance cost: ', medical_records)
+cheapest_three = medical_records[slice(3)]
+print('Here are the three cheapest insurance costs in our medical records: ', cheapest_three)
+priciest_three = medical_records[-3:]
+print('Here are the three most expensive insurance costs in our medical records: ', priciest_three)
+occurrences_paul = names.count('Paul')
+print('There are',occurrences_paul, 'people in our list called Paul')
